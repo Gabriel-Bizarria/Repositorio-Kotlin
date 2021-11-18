f211b2912a45264cea0930ccc7d9d23f5ecbe6ff
Gabriel Bizarria <gabrielsantos211000@gmail.com>
2021-11-18 16:15:58 -0300

    Ex. calculadora de gorjeta

---
+++ Tiptime/app/src/main/java/com/generation/tiptime/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.generation.tiptime
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.generation.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.calculateButton.setOnClickListener { calculateTip() }
+
+    }
+
+    fun calculateTip() {
+        val stringInTextField = binding.costOfService.text.toString()
+        val cost = stringInTextField.toDouble()
+        val selectedOption = binding.tipOptions.checkedRadioButtonId
+        val tipPercentage = when (selectedOption) {
+            R.id.option_twenty -> 0.20
+            R.id.option_eighteen -> 0.18
+            else -> 0.15
+        }
+        var tip = cost * tipPercentage
+        val roundUp = binding.roundUpSwitch.isChecked
+
+        if (roundUp) {
+            tip = kotlin.math.ceil(tip)
+        }
+
+        val finalTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount, finalTip)
+    }
+}
+
